086cf1c2e2c0abbbf5a2a87e302e8a17043813d7
llorensmarti <lgarcia@imvu.com>
2017-11-26 21:38:39 +0100

    Update ok.php

--- ok.php
+++ ok.php
@@ -1,11 +1,12 @@
 <?php
 $fuck = file($argv[1]);
+$filtre = $argv[2];
 foreach($fuck as $sites) {
 	$site=trim($sites);
 	echo "website => $sites\r";
 	sleep(2);
 	$config = @file_get_contents("http://".$site);
-	if(preg_match("/FILTRE/i",$config)) {
+	if(@preg_match("/$filtre/i",$config)) {
     file_put_contents("results.txt", "file find => $site\r\n",FILE_APPEND);
 	}
 	
